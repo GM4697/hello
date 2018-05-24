1124d8f20bbab1140d5ea2a628bd906a9e4e0186
GM4697 <39575574+GM4697@users.noreply.github.com>
2018-05-24 11:16:12 +0800

    Create string.cpp
    string类  +重载

---
+++ string.cpp
@@ -0,0 +1,22 @@
+string& operator+(string &str1,string& str2)
+{
+  char * temp = NULL;
+  int len = str1.len + str2.len;
+  temp = new char[len+1];
+  strcpy(temp,str1.rep);
+  strcat(temp,str2.rep);
+  string t = string(temp);
+  delete temp;
+  temp = NULL;
+  return t;
+}
+string & operator+=(string & str)
+{
+  char * temp = rep;
+  len = len + str.len;
+  rep = new char[len+1];
+  strcpy(rep,temp);
+  strcat(rep,str.rep);
+  delete temp;
+  return *this;
+}
